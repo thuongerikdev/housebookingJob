7bb9e86f6825216061efa59de59f880b8afee7e3
Nguyễn Xuân Thưởng <thuong0220766@huce.edu.vn>
2024-09-15 18:04:06 +0700

    update app

---
+++ HomeBookingJob/frontend/App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './App/Navigations/TabNavigation';
+import LoginNavigation from './App/Navigations/LoginNavigation'
+import BookingNavigation from './App/Navigations/BookingNavigation'
+import { useFonts } from 'expo-font';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+
+  });
+  return (
+
+    
+    <View style={styles.container}>
+    
+      <NavigationContainer>
+        <LoginNavigation/>
+       
+      </NavigationContainer>
+
+      <StatusBar style="auto" />
+      
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    
+  },
+});
